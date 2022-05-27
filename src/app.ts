74e98b756d3c452c7169c6e7229f35686c88715a
MatheusAltino <matheus@pop-os.localdomain>
2022-05-26 23:38:17 -0300

    :hammer: Clean App settings

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,11 @@
 import * as express from 'express'
-import * as bodyParser from 'body-parser'
 import * as cors from 'cors'
-
-import { AppDataSource } from './database/data-source'
-import { RouterUser } from './route/RouterUser'
+import RouterUser from './routes/RouterUser'
 
 export const app = express()
 
-//db connection
-AppDataSource.initialize()
-
 //config json
-app.use(bodyParser.json())
+app.use(express.json())
 
 //Acess control
 app.use(cors())
